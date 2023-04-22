dcacf4c9847b603cbf85d4e4dd9430c53d308959
Divjot Singh (bogas04) <bogas04@gmail.com>
2023-04-22 22:57:35 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <main className="flex flex-col py-6  bg-black text-white h-full gap-2">
+      <h1 className="text-blue-500 text-5xl">Hello World</h1>
+    </main>
+  );
+}
