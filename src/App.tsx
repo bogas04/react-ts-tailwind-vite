7d114665671ab6f0e7e760362c4b119e98899701
Divjot Singh (bogas04) <bogas04@gmail.com>
2023-04-28 15:46:11 +0530

    feat: add proper styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 export function App() {
   return (
-    <main className="flex flex-col py-6  bg-black text-white h-full gap-2">
+    <main className="flex flex-col flex-1 py-6 bg-black items-center text-white h-full gap-2">
       <h1 className="text-blue-500 text-5xl">Hello World</h1>
     </main>
   );
