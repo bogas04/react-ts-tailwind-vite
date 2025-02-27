0595feef59dd89e5b5ac718be0ed9033a858cf06
Divjot Singh <bogas04@gmail.com>
2025-02-27 13:49:58 +0530

    chore: time

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+
 export function App() {
+  const [time, setTime] = useState(Date.now());
+
+  useEffect(() => {
+    const timer = setInterval(() => setTime(Date.now()), 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <main className="flex flex-col flex-1 py-6 bg-black items-center text-white h-full gap-2">
       <h1 className="text-blue-500 text-5xl">Hello World</h1>
+      <p>Current time is {new Date(time).toLocaleTimeString("en-in")}</p>
     </main>
   );
 }
